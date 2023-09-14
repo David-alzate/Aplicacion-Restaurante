c303b802586100bd19726a79a44e064f0a3f0163
David-alzate <alzatedavid0126@gmail.com>
2023-09-14 11:04:29 -0500

    creamos archivos de clases

--- adminGeneral.py
+++ adminGeneral.py
@@ -1,17 +1,6 @@
 import psycopg2
 from usuario import*
 
-class adminGeneral:
-    def __init__(self, argumento):
-        self.argumento = argumento
-
-    def crearUsuario(conexion, nombre, apellido,correo,telefono,direccion,contraseña,id):
-        try:
-            with conexion.cursor() as cursor:
-                consulta = "INSERT INTO usuarios(nombre, apellido, correo, telefono, direccion, contraseña , id) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-                cursor.execute(consulta, (nombre, apellido, correo,telefono,direccion,contraseña,id))
-            conexion.commit()
-            return True
-        except psycopg2.Error as e:
-            print("Ocurrió un error al crear el usuario:     ", e)
-            return False
+class adminGeneral(usuario):
+    def __init__(self, nombre, apellido, telefono, direccion, contraseña, id, correo):
+        super().__init__(nombre, apellido, telefono, direccion, contraseña, id, correo)
--- main.py
+++ main.py
@@ -26,13 +26,13 @@ if __name__ == '__main__':
 
 
 
-# conexion = Basedatos("localhost","postgres","000")
-# conexion.conectar()
+conexion = Basedatos("localhost","postgres","000")
+conexion.conectar()
 
 
-# if (usuario.crearUsuario(conexion.conectar(), "luis", "ospina", "lector@gmail.com", "3127054715", "cll 15", "123", "1040031815")):
-#     print("USUARIO CREADO EXITOSAMENTE")
-# else:
-#     print("ERROR DE CREACION")
+if (usuario.crearUsuario(conexion.conectar(), "luis", "ospina", "lector@gmail.com", "3127054715", "cll 15", "123", "1040031815")):
+    print("USUARIO CREADO EXITOSAMENTE")
+else:
+    print("ERROR DE CREACION")
 
-# print("Hola David Alzate")
+print("Hola David Alzate")
