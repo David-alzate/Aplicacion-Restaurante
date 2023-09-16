dcc263b998a7ec0d9179de7f1668dee766034de6
David-alzate <alzatedavid0126@gmail.com>
2023-09-16 13:06:59 -0500

    inicio de session Prueba

--- login.py
+++ login.py
@@ -1,11 +1,33 @@
-class login:
-    def __init__(self, contraseña,id):
-        self.contraseña = contraseña
-        self.id = id
+import psycopg2
+from usuario import *
 
-    def iniciarSesion(self,contraseña,id):
-        pass
 
+class login(usuario):
+    def __init__(self,contraseña, id):
+        super().__init__(contraseña, id)
+    
+
+    def iniciarSesion(conexion, id, contraseña):
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM usuarios WHERE id = %s", (id,))
+                usuario = cursor.fetchone()
+                if usuario:
+                    # Comprobar si la contraseña coincide
+                    if contraseña == usuario[5]:   # 5 es el índice de la columna 'contraseña' en la base de datos 
+                        print("Inicio de sesión exitoso.")
+                        return True
+                    else:
+                        print("Contraseña incorrecta.")
+                        return False
+                else:
+                    print("El usuario no existe.")
+                    return False
+        except psycopg2.Error as e:
+            print("Ocurrió un error al consultar: ", e)
+            return False
+
+        
     def cerrarSesion(self):
         pass
         
--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 from database import *
 from adminGeneral import*
 from usuario import*
+from login import *
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
-# class EjemploGUI(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         uic.loadUi("ventanaPrincipal.ui",self)
-#         self.boton.clicked.connect(self.mostrarBoton)
-
-#     def mostrarBoton(self):
-#         print("Hola Mundo ")
-#         a = float(self.a.text())
-#         b = float(self.b.text())
-#         print(str(float(a)*float(b)))
-        
-
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     gui = EjemploGUI()
-#     gui.show()
-#     sys.exit((app.exec_()))
-
-
-
-
 conexion = Basedatos("localhost","postgres","000")
+
+class GUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ventanaPrincipal.ui",self)
+        self.boton.clicked.connect(self.login)
+
+    def login(self):
+        id = self.a.text()
+        contraseña = self.b.text()
+        
+        login.iniciarSesion(conexion.conectar(),id , contraseña)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gui = GUI()
+    gui.show()
+    sys.exit((app.exec_()))
+
+
+# login.iniciarSesion(conexion.conectar(),"1040031815","1040031815")
 
 
 
@@ -36,4 +37,4 @@ conexion = Basedatos("localhost","postgres","000")
 # else:
 #     print("ERROR DE CREACION")
 
-adminGeneral.eliminarUsuarios(conexion.conectar())
+# adminGeneral.eliminarUsuarios(conexion.conectar())
