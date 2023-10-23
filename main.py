c4101ef73253e6ec73af5e0f3d62ebfd1e2b728c
David-alzate <alzatedavid0126@gmail.com>
2023-10-23 17:39:18 -0500

    paguina principal

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from usuario import*
 from login import *
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QPushButton 
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QPropertyAnimation
+from PyQt5 import QtCore, QtWidgets
+import os
 
 # Realizamos la conexión a la base de datos
 conexion = Basedatos("localhost","postgres","000")
@@ -14,16 +18,72 @@ class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ventanaPrincipal.ui",self)
-        self.boton.clicked.connect(self.login)  # Al presionar el botón de la ventana llamar al método login
 
-# Método para logrearse
+        #Control de barra de titulos
+        self.btnNormalizar.hide()
+        self.btnCerrar.clicked.connect(lambda: self.close())
+        self.btnMaximizar.clicked.connect(self.crontrolBtMaximizar)
+        self.btnNormalizar.clicked.connect(self.crontrolBtNormalizar)
+        self.btnMinimizar.clicked.connect(self.crontrolBtMinimizar)
+
+        # Eliminar Barra de titulo y opacidad
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
+        #Redimensionar ventana
+        self.gripSize = 10
+        self.grip = QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize, self.gripSize)
+        # Mover Ventana 
+        self.frameSuperior.mouseMoveEvent = self.moverVentana
+        
+
+    # Método para logrearse
     def login(self):
         # Agregamos los datos ingresados en los QlineEdit a id y contraseña
         id = self.a.text()
         password = self.b.text()
         
         # llamamos el método iniciar sesión de login para verificar el inicio de sesión
-        login.iniciarSesion(conexion.conectar(),id , password)   
+        login.iniciarSesion(conexion.conectar(),id , password)
+
+    
+    # Control de barra de titulo
+    def crontrolBtMinimizar(self):
+       self.showMinimized()
+
+    def crontrolBtNormalizar(self):
+       self.showNormal()
+       self.btnNormalizar.hide()
+       self.btnMaximizar.show()
+      
+    def crontrolBtMaximizar(self):
+       self.showMaximized()
+       self.btnMaximizar.hide()  
+       self.btnNormalizar.show()
+    
+    # Redimensionar ventana 
+    def resizeEvent(self, event):
+       rect = self.rect()
+       self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+
+    # Mover ventana 
+    def mousePressEvent(self, event):
+       self.clickPosition = event.globalPos()
+
+    def moverVentana(self, event):
+       if self.isMaximized() == False:
+          if event.buttons() == QtCore.Qt.LeftButton:
+             self.move(self.pos() + event.globalPos() - self.clickPosition)
+             self.clickPosition = event.globalPos()
+             event.accept()
+       if event.globalPos().y() <= 10:
+          self.showMaximized()
+          self.btnMaximizar.hide()
+          self.btnNormalizar.show()
+       else:
+          self.showNormal()
+          self.btnNormalizar.hide()
+          self.btnMaximizar.show()
 
 
 if __name__ == '__main__':
