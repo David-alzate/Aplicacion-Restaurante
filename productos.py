66dddf6d782f55f6b4a3b404cd4b34b4b1177212
migue8104 <you@example.com>
2023-10-26 22:59:37 -0500

    el crud para los productos,los locales y sus clases tambien en database cmbio de la base a PRUEBA 2

---
+++ productos.py
@@ -0,0 +1,8 @@
+#clase producto
+
+class producto:
+    def __init__(self,id_producto,precio,nombreProducto):
+        self.id_producto=id_producto
+        self.precio=precio
+        self.nombreProducto=nombreProducto
+
